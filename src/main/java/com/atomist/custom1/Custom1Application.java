ba8ee4c9320b1d067937f49b3aaa55663c7388bc
Rod Johnson <johnsonroda@gmail.com>
2018-02-17 15:41:32 +1100

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/custom1/Custom1Application.java
@@ -0,0 +1,12 @@
+package com.atomist.custom1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Custom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Custom1Application.class, args);
+	}
+}
---
+++ src/test/java/com/atomist/custom1/Custom1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atomist.custom1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Custom1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
